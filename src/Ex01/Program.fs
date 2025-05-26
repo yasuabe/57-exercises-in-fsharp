0601d6e1ff0306f54a552347f6ee3f62cf4eb5d5
yasuabe2613 <yasu@yasuabe.net>
2025-05-26 23:17:49 +0900

    ex01: solve

---
+++ src/Ex01/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    Console.Write("What is your name? ")
+    let name     = Console.ReadLine()
+    let greeting = "Hello, " + name + ", nice to meet you!"
+    Console.WriteLine(greeting)
+    0
